4cb1c069f559b318bed4844550c734151e3db20b
Pichot Gaspard <droug.86@gmail.com>
2019-10-08 10:05:33 +0200

    add form

---
+++ src/Form/LieuxType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieux;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuxType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomLieu')
+            ->add('rue')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('ville')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieux::class,
+        ]);
+    }
+}
