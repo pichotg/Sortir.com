6c34f6e89fe47ca552eb42dd3a9202397cd56c66
Fuzip <victor.dittiere@icloud.com>
2019-10-08 11:27:58 +0200

    Twig bootrap & elements

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('main/home.html.twig', [
+            'app_name' => 'Home',
         ]);
     }
 }
